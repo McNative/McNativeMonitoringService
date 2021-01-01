60cf57c06a3efd14e710a0143594c8e80fe3e21c
Fridious <philippfriedhoff@outlook.de>
2021-01-01 18:54:20 +0100

    Updated maf sdk version and fixed server creation wrong name issue

--- src/main/java/org/mcnative/service/monitoring/listener/ServerStartupActionListener.java
+++ src/main/java/org/mcnative/service/monitoring/listener/ServerStartupActionListener.java
@@ -34,7 +34,7 @@ public class ServerStartupActionListener implements MAFActionListener<ServerStar
         boolean exists = resultEntry != null;
         if(exists) {
             this.monitoringService.getStorageService().getServerCollection().update()
-                    .set("Name", action.getName())
+                    .set("Name", action.getServerName())
                     .set("Address", action.getAddress().getHostName())
                     .set("Port", action.getAddress().getPort())
                     .set("ServerGroup", action.getServerGroup())
@@ -62,7 +62,7 @@ public class ServerStartupActionListener implements MAFActionListener<ServerStar
             this.monitoringService.getStorageService().getServerCollection().insert()
                     .set("Id", executor.getClientId().toString())
                     .set("NetworkId", executor.getNetworkId().toString())
-                    .set("Name", action.getName())
+                    .set("Name", action.getServerName())
                     .set("Address", action.getAddress().getHostName())
                     .set("Port", action.getAddress().getPort())
                     .set("ServerGroup", action.getServerGroup())
